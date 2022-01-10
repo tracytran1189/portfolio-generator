ba9a5c241235895bebf11bfeef41ba31991dd0ae
Tracy Tran <tracytran1189@gmail.com>
2022-01-09 21:35:49 -0600

    create index.html

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArgs => {
-  profileDataArgs.forEach(profileItem => console.log(profileItem));
- 
+const profileDataArgs = process.argv.slice(2,process.argv.length);
+// console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = (name, github) => {
+return `
+<!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+`;
 };
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portofolio complete! Check out index.html to see the output!')
+});
+
+// printProfileData(profileDataArgs);
