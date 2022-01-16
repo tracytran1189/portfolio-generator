4ce218000c419bcc0626b6585b7b9ccb17b8ffe2
Tracy Tran <tracytran1189@gmail.com>
2022-01-16 17:04:12 -0600

    add template to html file

--- app.js
+++ app.js
@@ -134,17 +134,10 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-    });
+        const pageHTML = generatePage(portfolioData);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
 
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portofolio complete! Check out index.html to see the output!')
-// });
-
+    console.log('Portofolio complete! Check out index.html to see the output!')
+});
+});
